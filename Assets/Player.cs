822d74ebded214f70b0530c203bdf04292c0f9b6
AJW8 <aaron32@gmail.com>
2023-09-07 13:28:10 +1200

    Commit from terminal

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -13,11 +13,14 @@ public class Player : MonoBehaviour
     public float jumpMultiplier;
     public float fallMultiplier;
     private Rigidbody2D rigidBody;
+    private float camZ;
     private float walkInput;
     private bool jumpInput;
     private bool onGround;
     private bool flipInput;
     private bool horizonFlipped;
+    private float flipFactor;
+    private bool flipMiddle;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,7 @@ public class Player : MonoBehaviour
         colliderWhite.SetActive(false);
         backgroundBlack.SetActive(false);
         rigidBody = GetComponent<Rigidbody2D>();
+        camZ = Mathf.Abs(Camera.main.transform.position.z);
         onGround = true;
         horizonFlipped = false;
     }
@@ -39,32 +43,49 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (horizonFlipped ? flipFactor < 1 : flipFactor > 0)
+        {
+            Transform cam = Camera.main.transform;
+            if (horizonFlipped)
+            {
+                flipFactor += Time.deltaTime;
+                if (flipFactor >= 1) flipFactor = 1;
+                else if (!flipMiddle && flipFactor >= 0.5f)
+                {
+                    colliderBlack.SetActive(false);
+                    colliderWhite.SetActive(true);
+                    backgroundBlack.SetActive(true);
+                    backgroundWhite.SetActive(false);
+                    rigidBody.gravityScale *= -1;
+                    flipMiddle = true;
+                }
+            }
+            else
+            {
+                flipFactor -= Time.deltaTime;
+                if (flipFactor <= 0) flipFactor = 0;
+                else if (!flipMiddle && flipFactor <= 0.5f)
+                {
+                    colliderWhite.SetActive(false);
+                    colliderBlack.SetActive(true);
+                    backgroundBlack.SetActive(false);
+                    backgroundWhite.SetActive(true);
+                    rigidBody.gravityScale *= -1;
+                    flipMiddle = true;
+                }
+            }
+            cam.position = new Vector3(cam.position.x, cam.position.y, camZ * Mathf.Sin(Mathf.PI * (flipFactor * 2 - 1) / 2));
+            cam.rotation = Quaternion.Slerp(Quaternion.LookRotation(Vector3.forward, Vector2.up), Quaternion.LookRotation(-Vector3.forward, -Vector2.up), flipFactor);
+            return;
+        }
         if (Mathf.Abs(walkInput) > 0.1f) rigidBody.AddForce(Vector2.right * walkInput * walkSpeed, ForceMode2D.Impulse);
         if (onGround && jumpInput) rigidBody.AddForce(Vector2.up * jumpForce * (horizonFlipped ? -1 : 1), ForceMode2D.Impulse);
         if (horizonFlipped ? rigidBody.velocity.y > 0 : rigidBody.velocity.y < 0) rigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * (horizonFlipped ? -1 : 1) * Time.deltaTime;
         else if (horizonFlipped ? rigidBody.velocity.y < 0 : rigidBody.velocity.y > 0) rigidBody.velocity += Vector2.up * Physics2D.gravity.y * (jumpMultiplier - 1) * (horizonFlipped ? -1 : 1) * Time.deltaTime;
         if (onGround && flipInput)
         {
-            Transform cam = Camera.main.transform;
-            cam.position = new Vector3(cam.position.x, cam.position.y, -cam.position.z);
-            if (horizonFlipped)
-            {
-                cam.rotation = Quaternion.LookRotation(Vector3.forward, Vector2.up);
-                colliderWhite.SetActive(false);
-                colliderBlack.SetActive(true);
-                backgroundBlack.SetActive(false);
-                backgroundWhite.SetActive(true);
-            }
-            else
-            {
-                cam.rotation = Quaternion.LookRotation(-Vector3.forward, -Vector2.up);
-                colliderBlack.SetActive(false);
-                colliderWhite.SetActive(true);
-                backgroundBlack.SetActive(true);
-                backgroundWhite.SetActive(false);
-            }
-            rigidBody.gravityScale *= -1;
             horizonFlipped = !horizonFlipped;
+            flipMiddle = false;
         }
     }
 
--- README.md
+++ README.md
@@ -3,5 +3,5 @@ A proof of concept for a 2D platforming game created in Unity game engine.
 
 Controls:
 * Left/right arrow keys - move across
-* Up arrow key - jump
-* Down arrow key - flip the horizon
+* Up arrow key (while on ground) - jump
+* Down arrow key (while on ground) - flip the horizon
